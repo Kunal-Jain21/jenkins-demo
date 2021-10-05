c46e730dadf3b00cf9709fcdcc9a3c498c226909
Kunal Jain <79712735+Kunal-Jain21@users.noreply.github.com>
2021-10-05 16:01:21 +0530

    Jenkins commit1

---
+++ job1.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+class job1 {
+	public static void main(String[] args) {
+		System.out.println("Job1 done");
+	}
+}
---
+++ job2.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+class job2 {
+	public static void main(String[] args) {
+		System.out.println("Job2 done");
+	}
+}
